e7f8211afc5c2e682c9af5587e7107591b0b8c7e
guigail <guillaume.crespel@gmail.com>
2016-08-16 11:14:06 +0200

    new Branche toolbox

--- src/main/js/components/App/App.jsx
+++ src/main/js/components/App/App.jsx
@@ -1,12 +1,17 @@
 import React from 'react'
+import AppBar from 'react-toolbox/lib/app_bar'
 import Tvshows from '../Tvshows'
 import Search from '../Search'
 
-const Component = () => (
-  <div>
-    <Search />
-    <Tvshows />
-  </div>
-)
+const Component = () => {
+  return (
+    <div className="app">
+      <AppBar fixed >
+        <Search />
+      </AppBar>
+      <Tvshows />
+    </div>
+  )
+}
 
 export default Component
--- src/main/js/components/Search/Search.jsx
+++ src/main/js/components/Search/Search.jsx
@@ -1,10 +1,13 @@
 import React, { PropTypes } from 'react'
+import Input from 'react-toolbox/lib/input'
 
-const Component = ({ search }) => (
-  <div>
-    <input onChange={search} />
-  </div>
-)
+const Component = ({ search }) => {
+  return (
+    <div>
+      <Input type="text" label="Recherche" icon="search" onChange={ search } />
+    </div>
+  )
+}
 
 Component.propTypes = {
   search: PropTypes.func.isRequired,
